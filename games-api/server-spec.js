a5f8f8b1a02fe8d76bea43fcdc2ba6807d594898
Antony Nyagah <anttonynyagah@Antonys-MBP-2.attlocal.net>
2019-07-20 15:21:29 -0400

    get and post created, migrations and knexfile edited

--- games-api/server-spec.js
+++ games-api/server-spec.js
@@ -1,12 +1,84 @@
 const server = require('./server');
 
+const Games = require('../games-model/gamesModel');
+
+const db = require('../data/dbConfig');
+
 const request = require('supertest');
 
-describe('GET /', () => {
+const mario = {
+  id: 1,
+  title: "Mario's Dream Land",
+  genre: 'Platform',
+  releaseYear: 1994
+};
+
+const randomGame = {
+  title: 'missing strikes again'
+}
+
+    describe('GET /', () => {
+
     it('should return status 200', async () => {
 
       const res = await request(server).get('/');
 
       expect(res.status).toBe(200);
+    });
+
+    describe('GET /games', () => {
+      beforeEach(() => {
+
+      return db('games').truncate();
+    });
+
+    it('should return http status 200', async () => {
+      const res = await request(server).get('/games');
+
+    expect(res.status).toBe(200);
+    });
+
+    it('should return list of games', async () => {
+      const res = await request(server).get('/games');
+
+    expect(res.body).toEqual([]);
+    });
+
+    it('should return empty array when no games are listed', async () => {
+        const res = await request(server).get('/games');
+      // removed seed data, as at time of writing no other games exist in table
+      await Games.remove(0);
+      await Games.remove(1);
+      await Games.remove(2);
+
+      expect(res.body).toEqual([]);
+      });
+    });
   });
+
+
+  describe('POST /games', () => {
+    beforeEach(() => {
+      return db('games').truncate();
+  });
+
+    it('should post a new game and return status 201', async () => {
+    let res = await request(server).post('/games')
+      .send(mario)
+
+    expect(res.body).toEqual(mario);
+  });
+
+    it('should return staus 201', async () => {
+      let res = await request(server).post('/games')
+      .send(mario)
+
+      expect(res.status).toBe(201);
+    });
+
+    it('should return status 421 if game info is missing', async () => {
+      let res = await request(server).post('/games')
+      .send(randomGame)
+      expect(res.status).toBe(421);
+    });
 });
--- games-api/server.js
+++ games-api/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const Games = require('../games-model/gamesModel');
+
 const server = express();
 
 server.use(express.json());
@@ -8,4 +10,20 @@ server.get('/', async (req,res) => {
   res.status(200).json({ message: 'Welcome to the games API Sprint testing challenge'})
 });
 
+server.get('/games', async (req, res) => {
+  const games = await Games.getAll();
+  res.status(200).json(games)
+});
+
+server.post('/games', async (req, res) => {
+  try {
+    const newGame = await Games.add(req.body)
+
+    res.status(201).json(newGame)
+  } catch (error) {
+    res.status(421).json(error)
+  }
+});
+
 module.exports = server;
+
